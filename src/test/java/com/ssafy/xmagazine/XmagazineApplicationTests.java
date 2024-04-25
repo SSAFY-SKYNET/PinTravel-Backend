adc46f2fcbaa4c5857f248e194ad52aa6819e583
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-25 16:46:20 +0900

    initial commit

---
+++ src/test/java/com/ssafy/xmagazine/XmagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.xmagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
