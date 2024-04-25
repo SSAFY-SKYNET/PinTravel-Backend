21ce0b90f4926464fab392501dc810a9f759e4ba
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-25 16:46:20 +0900

    initial commit

---
+++ src/main/java/com/ssafy/xmagazine/XmagazineApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.xmagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmagazineApplication.class, args);
+	}
+
+}
