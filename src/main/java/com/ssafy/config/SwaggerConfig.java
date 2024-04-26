e1fe8a1febac4002f9094019a216a5a8fc756110
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-26 13:06:30 +0900

    🎨 add swagger config

---
+++ src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.ssafy.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info().title("XMAGAZINE API").description("XMAGAZINE API 문서").version("v1");
+
+        return new OpenAPI().components(new Components()).info(info);
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/user/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-admin")
+                .pathsToMatch("/admin/**")
+                .build();
+    }
+}
