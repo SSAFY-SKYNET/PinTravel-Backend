f11a84b2f5077a3716eda30d8e26439d47573462
Picka <65598179+JAEKWANG97@users.noreply.github.com>
2024-04-26 11:45:49 +0900

    🌱 add UserService interface

--- src/main/java/com/ssafy/service/UserService.java
+++ src/main/java/com/ssafy/service/UserService.java
@@ -1,5 +1,19 @@
 package com.ssafy.service;
 
+import java.util.List;
+
+import com.ssafy.dto.UserDto;
+
 public interface UserService {
 
+    // 기본 CRUD
+    public void insertUser(UserDto userDto);
+
+    public void updateUser(UserDto userDto);
+
+    public void deleteUser(int userId);
+
+    public UserDto getUser(int userId);
+
+    public List<UserDto> getAllUser();
 }
